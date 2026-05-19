a3ffb1bce60a61ea574cbbd57cad366aff9f6f48
aliiranmanesh <ali.ir.dev@gmail.com>
2026-05-19 11:35:20 +0330

    feat: added views for index, contact, about

--- website/views.py
+++ website/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(requests):
+    return render(requests, 'website/index.html')
+
+def about(requests):
+    return render(requests, 'website/about.html')
+
+def contact(requests):
+    return render(requests, 'website/contact.html')
